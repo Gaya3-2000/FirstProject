555cc4268e779e3e5100744c84e7703c73f9dab7
GAYATHRI <GAYATHRI@LAPTOP-12T065R4>
2022-11-07 12:40:40 +0530

    Mvc Commit

--- MvcDemo/src/main/java/com/springBoot/Mvcdemo/repository/StudentRepository.java
+++ MvcDemo/src/main/java/com/springBoot/Mvcdemo/repository/StudentRepository.java
@@ -1,9 +1,10 @@
 package com.springBoot.Mvcdemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springBoot.Mvcdemo.entity.Student;
-
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Long>{
 
 }
